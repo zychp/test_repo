d03e78d64288e7e3b10b83ba5fe1bdbfac25825a
ZYCHP <pawel.zych@contractors.roche.com>
2024-08-29 13:10:37 +0200

    add import numpy and print median

--- first_python.py
+++ first_python.py
@@ -1,5 +1,11 @@
 # Display the output
 my_variable = 57
+x1 = 1
+x2 = 2
+x3 = 6
+my_average = x1 + x2 + x3
+my_average = my_average / 3
+
 print("This is a New Python File")
 print(f"This is a New Python File displaying {my_variable}")
-
+print(f"This is a New Python File displaying {my_average}")
