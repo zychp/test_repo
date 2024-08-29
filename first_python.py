4e5e73fa67e3e291fc5150d5208cf739afb6f941
ZYCHP <pawel.zych@contractors.roche.com>
2024-08-29 13:29:44 +0200

    add median calculation

--- first_python.py
+++ first_python.py
@@ -1,11 +1,14 @@
 # Display the output
+import numpy as np
 my_variable = 57
 x1 = 1
 x2 = 2
 x3 = 6
 my_average = x1 + x2 + x3
 my_average = my_average / 3
+my_median = np.median([x1, x2, x3])
 
 print("This is a New Python File")
 print(f"This is a New Python File displaying {my_variable}")
 print(f"This is a New Python File displaying {my_average}")
+print(f"This is a New Python File displaying {my_median}")
